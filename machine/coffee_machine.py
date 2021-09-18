e674f527dec56986fd91909f4645a9eadf629ef3
Pedro Calleja <pecalleja@gmail.com>
2021-09-18 13:14:59 -0400

    5/6

--- machine/coffee_machine.py
+++ machine/coffee_machine.py
@@ -48,29 +48,42 @@ coffee_machine = CoffeeMachine(
     coffee_beams=120,
     disposable_cups=9
 )
-coffee_machine.get_status()
-print("Write action (buy, fill, take):")
-action = input()
-if action == "fill":
-    print("Write how many ml of water you want to add:")
-    coffee_machine.water += int(input())
-    print("Write how many ml of milk you want to add:")
-    coffee_machine.milk += int(input())
-    print("Write how many grams of coffee beans you want to add:")
-    coffee_machine.coffee_beams += int(input())
-    print("Write how many disposable coffee cups you want to add:")
-    coffee_machine.disposable_cups += int(input())
-elif action == "take":
-    print(f"I gave you {coffee_machine.cash}")
-    coffee_machine.cash = 0
-elif action == "buy":
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    coffee_option = input()
-    recipe = coffee_machine.recipes[coffee_option]
-    coffee_machine.cash += recipe["cost"]
-    coffee_machine.milk -= recipe["milk"]
-    coffee_machine.water -= recipe["water"]
-    coffee_machine.coffee_beams -= recipe["coffee"]
-    coffee_machine.disposable_cups -= 1
 
-coffee_machine.get_status()
+print("Write action (buy, fill, take, remaining, exit):")
+action = input()
+while action != "exit":
+    if action == "fill":
+        print("Write how many ml of water you want to add:")
+        coffee_machine.water += int(input())
+        print("Write how many ml of milk you want to add:")
+        coffee_machine.milk += int(input())
+        print("Write how many grams of coffee beans you want to add:")
+        coffee_machine.coffee_beams += int(input())
+        print("Write how many disposable coffee cups you want to add:")
+        coffee_machine.disposable_cups += int(input())
+    elif action == "take":
+        print(f"I gave you {coffee_machine.cash}")
+        coffee_machine.cash = 0
+    elif action == "buy":
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        coffee_option = input()
+        if coffee_option != "back":
+            recipe = coffee_machine.recipes[coffee_option]
+            if coffee_machine.milk < recipe["milk"]:
+                print("Sorry, not enough milk!")
+            elif coffee_machine.water < recipe["water"]:
+                print("Sorry, not enough water!")
+            elif coffee_machine.coffee_beams < recipe["coffee"]:
+                print("Sorry, not enough coffee!")
+            else:
+                print("I have enough resources, making you a coffee!")
+                coffee_machine.cash += recipe["cost"]
+                coffee_machine.milk -= recipe["milk"]
+                coffee_machine.water -= recipe["water"]
+                coffee_machine.coffee_beams -= recipe["coffee"]
+                coffee_machine.disposable_cups -= 1
+    elif action == "remaining":
+        coffee_machine.get_status()
+
+    print("Write action (buy, fill, take, remaining, exit):")
+    action = input()
